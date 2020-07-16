638d856b81d19ccfde3f18e5b7bfe3735f7eb1d7
claudette <honey20103@yahoo.com>
2020-07-16 22:00:29 +0000

    added recipe content from lorem ipsum

--- assets/js/connectform.js
+++ assets/js/connectform.js
@@ -1,36 +1,41 @@
 // Disable form submissions if there are invalid fields
-(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Collects the form and then adds validation styles 
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Collects the form and then adds validation styles
+      var forms = document.getElementsByClassName("needs-validation");
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
 })();
 
-
 // Name of the file being uploaded to appear on selection
-$(".custom-file-input").on("change", function() {
+$(".custom-file-input").on("change", function () {
   var fileName = $(this).val().split("\\").pop();
   $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
 });
 
-
-
 // Clear form fields after submission
 function submitForm() {
-    document.getElementById("subscription").reset();
-    document.getElementById("from_name").value = null;;
-    document.getElementById("from_email").value = null;;
-    document.getElementById("image").reset();
-    alert ("Your recipe has been sent :)")
+  document.getElementById("subscription").reset();
+  document.getElementById("from_name").value = null;
+  document.getElementById("from_email").value = null;
+  document.getElementById("image").reset();
+  alert("Your recipe has been sent :)");
 }
--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,8 +2,8 @@ function initMap() {
   var map = new google.maps.Map(document.getElementByID("map"), {
     zoom: 3,
     center: {
-      lat: 46.619261,
-      lng: -33.134766,
+      lat: 46.619865,
+      lng: -43.156866,
     },
   });
 }
